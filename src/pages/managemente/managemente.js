58fd73b4fe2b29af354ac3b1084de4bb369bf11d
eduardo2382 <motadossantosjoseeduardo.m82@gmail.com>
2024-07-08 20:21:03 -0300

    testing keyboartesting keyboard layout on mobiled layout on mobile

--- src/pages/managemente/managemente.js
+++ src/pages/managemente/managemente.js
@@ -1,3 +1,35 @@
 window.onpopstate = () => {
     window.history.pushState(null, '', window.location.href);
 }
+
+var currentSection = 'left'
+const btnSectionKeyboard = document.querySelector('#btnSectionKeyboard')
+const btnSectionMenu = document.querySelector('#btnSectionMenu')
+
+btnSectionKeyboard.addEventListener('click',()=>{
+    let state = currentSection == 'left'
+    let bar = document.querySelector('#barFooterSection')
+
+    if(!state){
+        currentSection = 'left'
+        bar.classList = 'footerSectionLeft'
+    }
+})
+
+btnSectionMenu.addEventListener('click',()=>{
+    let state = currentSection == 'left'
+    let bar = document.querySelector('#barFooterSection')
+
+    if(state){
+        currentSection = 'right'
+        bar.classList = 'footerSectionRight'
+    }
+})
+
+function showElement(element){
+    element.style.display = 'block'
+}
+
+function hideElement(element){
+    element.style.display = 'none'
+}
