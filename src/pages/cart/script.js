7762c5341eab3298caba4074b4ec4f49ad923d91
eduardo2382 <motadossantosjoseeduardo.m82@gmail.com>
2024-07-16 20:07:49 -0300

    system to modify the quantity of products created

--- src/pages/cart/script.js
+++ src/pages/cart/script.js
@@ -9,7 +9,7 @@ var cart = JSON.parse(sessionStorage.getItem('cart'))
 
 var productsList = document.querySelector('.productsList')
 
-var request = {}
+var request = []
 
 const payments = document.querySelectorAll('.payments')
 
@@ -28,13 +28,14 @@ function loadProducts(){
     productsList.innerHTML = ''
 
     cart.forEach((product)=>{
-        console.log(cart)
         createProductElement(product, product.id)
     })
 
     request.products = cart
 
-    observeDeleteProduct()
+    updateTotal()
+    observeAdd()
+    observeSubtract()
 }
 
 loadProducts()
@@ -43,48 +44,141 @@ function createProductElement(product, id){
     let productElement = document.createElement('div')
     productElement.setAttribute('class', 'products')
     
+    let spanProduct = document.createElement('div')
+    spanProduct.setAttribute('class', 'spanProduct')
+
     let productName = document.createElement('span')
     productName.setAttribute('class', 'productName')
     productName.innerText = product.name
-
-    let productValue = document.createElement('span')
-    productValue.setAttribute('class', 'valueProduct')
-    productValue.innerText = `R$ ${product.value}`
-    productName.appendChild(productValue)
-
-    productElement.appendChild(productName)
-
-
-    let spanButtons = document.createElement('span')
-    spanButtons.setAttribute('class', 'spanButtons')
 
     let btnObservation = document.createElement('button')
     btnObservation.setAttribute('class', 'btnObservation')
     btnObservation.setAttribute('key', id)
     let iconObservation = document.createElement('i')
-    iconObservation.setAttribute('class', 'ri-edit-box-line')
+    iconObservation.setAttribute('class', 'ri-pencil-fill')
     btnObservation.appendChild(iconObservation)
-    spanButtons.appendChild(btnObservation)
 
-    let btnDelete = document.createElement('button')
-    btnDelete.setAttribute('class', 'btnDelete')
-    btnDelete.setAttribute('key', id)
-    let iconDelete = document.createElement('i')
-    iconDelete.setAttribute('class', 'ri-delete-bin-line')
-    btnDelete.appendChild(iconDelete)
-    spanButtons.appendChild(btnDelete)
+    productName.appendChild(btnObservation)
+    spanProduct.appendChild(productName)
+    
 
-    productElement.appendChild(spanButtons)
+    let productValue = document.createElement('span')
+    productValue.setAttribute('class', 'valueProduct')
+    productValue.innerText = `R$ ${product.value}`
+    spanProduct.appendChild(productValue)
+
+    spanProduct.appendChild(productValue)
+
+    productElement.appendChild(spanProduct)
+
+
+    let spanValue = document.createElement('span')
+    spanValue.setAttribute('class', 'spanValue')
+
+    let btnAdd = document.createElement('button')
+    btnAdd.setAttribute('class', 'btnAdd')
+    btnAdd.setAttribute('key', id)
+    let iconAdd = document.createElement('i')
+    iconAdd.setAttribute('class', 'ri-add-line')
+    btnAdd.appendChild(iconAdd)
+
+    let productQuantity = document.createElement('span')
+    productQuantity.setAttribute('class', 'productQuantity')
+    productQuantity.setAttribute('key', id)
+    productQuantity.innerText = product.quantity
+
+    let btnSubtract = document.createElement('button')
+    btnSubtract.setAttribute('class', 'btnSubtract')
+    btnSubtract.setAttribute('key', id)
+    let iconSubtract = document.createElement('i')
+    iconSubtract.setAttribute('class', 'ri-subtract-line')
+    btnSubtract.appendChild(iconSubtract)
+
+    spanValue.appendChild(btnSubtract)
+    spanValue.appendChild(productQuantity)
+    spanValue.appendChild(btnAdd)
+    productElement.appendChild(spanValue)
+
     productsList.appendChild(productElement)
 }
 
-function observeDeleteProduct(){
-    let buttons = document.querySelectorAll('.btnDelete')
+function updateTotal(){
+    let totalValue = document.querySelector(".totalValue")
 
-    buttons.forEach((button)=>{
+    let value = 0
+
+    cart.forEach((product)=>{
+        value = value + (product.value * product.quantity)
+    })
+
+    totalValue.innerText = `R$ ${value.toFixed(2)}`
+}
+
+function observeAdd(){
+    let buttonsAdd = document.querySelectorAll('.btnAdd')
+    let productsQuantity = document.querySelectorAll('.productQuantity')
+    
+    buttonsAdd.forEach((button)=>{
+        let buttonKey = button.getAttribute('key')
+
         button.addEventListener('click', ()=>{
-            cart = cart.filter(item => item.id != button.getAttribute('key'))
-            loadProducts()
+            productsQuantity.forEach((element)=>{
+                let elementKey = element.getAttribute('key')
+
+                if(elementKey == buttonKey){
+                    cart.forEach((item)=>{
+                        if(item.id == buttonKey){
+                            item.quantity++
+                            element.innerText = item.quantity
+                        }
+                    })
+                }
+            })
+
+            updateTotal()
+            updateCart()
+            
         })
+    })
+}
+
+function observeSubtract(){
+    let buttonsSubtract = document.querySelectorAll('.btnSubtract')
+    let productsQuantity = document.querySelectorAll('.productQuantity')
+
+    buttonsSubtract.forEach((button)=>{
+        let buttonKey = button.getAttribute('key')
+
+        button.addEventListener('click', ()=>{
+            productsQuantity.forEach((element)=>{
+                let elementKey = element.getAttribute('key')
+
+                if(elementKey == buttonKey){
+                    cart.forEach((item)=>{
+                        if(item.id == buttonKey){
+                            if(item.quantity > 0){
+                                item.quantity--
+                                element.innerText = item.quantity
+                            } 
+                        }
+                    })
+                }
+            })
+
+            updateTotal()  
+            updateCart()
+        })
+    })
+}
+
+function updateCart(){
+    sessionStorage.setItem('cart', JSON.stringify(cart))
+}
+
+function updateRequest(){
+    request = {}
+
+    cart.forEach((item)=>{  
+
     })
 }
