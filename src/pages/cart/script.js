fc5bff3c229c1dbfed1ba260256498b2b499a81f
eduardo2382 <motadossantosjoseeduardo.m82@gmail.com>
2024-07-16 12:32:34 -0300

    added the value of products and footer

--- src/pages/cart/script.js
+++ src/pages/cart/script.js
@@ -9,33 +9,58 @@ var cart = JSON.parse(sessionStorage.getItem('cart'))
 
 var productsList = document.querySelector('.productsList')
 
+var request = {}
 
+const payments = document.querySelectorAll('.payments')
+
+payments.forEach((button)=>{
+    button.addEventListener('click', ()=>{
+        payments.forEach((payment)=>{
+            payment.classList.remove('active')
+        })
+        button.classList.toggle('active')
+
+        request.methodPayment = button.getAttribute('key')
+    })
+})
 
 function loadProducts(){
-    
+    productsList.innerHTML = ''
 
     cart.forEach((product)=>{
         console.log(cart)
-        createProductElement(product)
+        createProductElement(product, product.id)
     })
+
+    request.products = cart
+
+    observeDeleteProduct()
 }
 
 loadProducts()
 
-function createProductElement(product){
+function createProductElement(product, id){
     let productElement = document.createElement('div')
     productElement.setAttribute('class', 'products')
     
     let productName = document.createElement('span')
     productName.setAttribute('class', 'productName')
     productName.innerText = product.name
+
+    let productValue = document.createElement('span')
+    productValue.setAttribute('class', 'valueProduct')
+    productValue.innerText = `R$ ${product.value}`
+    productName.appendChild(productValue)
+
     productElement.appendChild(productName)
+
 
     let spanButtons = document.createElement('span')
     spanButtons.setAttribute('class', 'spanButtons')
 
     let btnObservation = document.createElement('button')
     btnObservation.setAttribute('class', 'btnObservation')
+    btnObservation.setAttribute('key', id)
     let iconObservation = document.createElement('i')
     iconObservation.setAttribute('class', 'ri-edit-box-line')
     btnObservation.appendChild(iconObservation)
@@ -43,6 +68,7 @@ function createProductElement(product){
 
     let btnDelete = document.createElement('button')
     btnDelete.setAttribute('class', 'btnDelete')
+    btnDelete.setAttribute('key', id)
     let iconDelete = document.createElement('i')
     iconDelete.setAttribute('class', 'ri-delete-bin-line')
     btnDelete.appendChild(iconDelete)
@@ -50,4 +76,15 @@ function createProductElement(product){
 
     productElement.appendChild(spanButtons)
     productsList.appendChild(productElement)
+}
+
+function observeDeleteProduct(){
+    let buttons = document.querySelectorAll('.btnDelete')
+
+    buttons.forEach((button)=>{
+        button.addEventListener('click', ()=>{
+            cart = cart.filter(item => item.id != button.getAttribute('key'))
+            loadProducts()
+        })
+    })
 }
